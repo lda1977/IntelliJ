f16987125f2e35078ce3d88ceaa1d65189588022
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-16 16:26:05 -0300

    Atributos Static, onde deve acessar com a Class e nao com This

--- src/br/com/lda/javacore/Fmodificadorestatico/Classes/Carro.java
+++ src/br/com/lda/javacore/Fmodificadorestatico/Classes/Carro.java
@@ -4,68 +4,60 @@ public class Carro {
     // Velocidade Limite é 240 Km/h
     /* Atributos */
     private String nome;
-    private String data;
-    private String modelo;
-    private String serie;
     private double velocidadeMaxima;
+    /* Membro da Classe não é mais do OBJ */
     public static double velocidadeLimite = 240;
 
-    /* Construtor */
-    public Carro(String nomeP, String dataP, String modeloP, String serieP, double velocidadeMaximaP) {
-        this.nome = nomeP;
-        this.data = dataP;
-        this.modelo = modeloP;
-        this.serie = serieP;
-        this.velocidadeMaxima = velocidadeMaximaP;
+    /***************
+     * Metodo Imprime
+     * *************/
+    public void imprime() {
+        System.out.println("_______________");
+        System.out.println("Nome " + this.nome);
+        System.out.println("VelMaxima " + this.velocidadeMaxima);
+        System.out.println("VelLimite " + this.velocidadeLimite);
+    }
+
+    /**************
+     * Construtores
+     * ************/
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
     }
 
     public Carro() {
-
     }
 
-    public void imprime() {
-        //System.out.println("--------------------");
-        System.out.println("-Marca " + this.nome);
-        //System.out.println("Data de Fabricação " + this.data);
-        //System.out.println("Modelo " + this.modelo);
-        //System.out.println("Serie " + this.serie);
-        System.out.println("Velocidade Máxima " + this.velocidadeMaxima);
-        //System.out.println("Velocidade Limite " + this.velocidadeLimite);
-        System.out.println("Velocidade Limite " + velocidadeLimite);
+    /*************
+     * Metodos Set
+     * ***********/
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    /* Metodos Set and Get */
-    /* SET */
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nomeP) {
-        this.nome = nomeP;
-    }
-    public void setData(String dataP) {
-        this.data = dataP;
-    }
-    public void setModelo(String modeloP) {
-        this.modelo = modeloP;
-    }
-    public void setSerie(String serieP) {
-        this.serie = serieP;
-    }
-
-    /* GET */
-    public String getData() {
-        return data;
-    }
-    public String getModelo() {
-        return modelo;
-    }
-    public String getSerie() {
-        return serie;
-    }
-    public double getVelocidadeMaxima() {
-        return velocidadeMaxima;
-    }
     public void setVelocidadeMaxima(double velocidadeMaxima) {
         this.velocidadeMaxima = velocidadeMaxima;
     }
+
+    /*
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+    /*
+    /**************
+     * Metodos Get
+     * ***********/
+    public String getNome() {
+        return nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+    /*
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+    */
 }
--- src/br/com/lda/javacore/Fmodificadorestatico/Test/CarroTest.java
+++ src/br/com/lda/javacore/Fmodificadorestatico/Test/CarroTest.java
@@ -4,17 +4,18 @@ import br.com.lda.javacore.Fmodificadorestatico.Classes.Carro;
 
 public class CarroTest {
     public static void main(String[] args){
-        Carro _objA = new Carro("BMW","02/12/2020","330", "3", 260);
-        Carro _objB = new Carro("Audi","01/12/2011","A3", "3", 250 );
-        Carro _objC = new Carro("Mercedis","03/02/2020","S4","4", 280);
+        Carro _objA = new Carro("BMW", 260);
+        Carro _objB = new Carro("Audi", 250 );
+        Carro _objC = new Carro("Mercedis", 280);
 
         _objA.imprime();
         _objB.imprime();
         _objC.imprime();
 
-        /* Modificando a variavel Static */
-        _objA.velocidadeLimite = 180;
-        Carro.velocidadeLimite = 200;
+        System.out.println("***************");
+
+        Carro.velocidadeLimite = 220;
+        //_objA.setVelocidadeLimite(220);
 
         _objA.imprime();
         _objB.imprime();
