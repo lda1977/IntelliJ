cb52412752972f3a24b5240d5d503815fe490c42
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-17 19:21:31 -0300

    add ToString Default

--- src/br/com/lda/javacore/Isobrescrita/Classes/Pessoa.java
+++ src/br/com/lda/javacore/Isobrescrita/Classes/Pessoa.java
@@ -4,12 +4,22 @@ public class Pessoa {
     private String nome;
     private int idade;
 
-    @Override
-    /* Classe do Objcte Java */
-    public String toString(){
 
-        return "Nome "+nome+" Idade "+idade;
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
     }
+
+    //
+//    @Override
+//    /* Classe do Objcte Java */
+//    public String toString(){
+//
+//        return "Nome "+nome+" Idade "+idade;
+//    }
 
 
 
